094f47ea400c6393276a2e17a6a4a65386bdbbd3
Jack Packham <150432276+Jack-Packham@users.noreply.github.com>
2024-02-14 15:47:53 +0000

    Revert "FPAD-1771-RPT-ConfigParams added to Generator + support classes (#7)"
    This reverts commit 49394de07355cd1f026ece511ddc434c5b340f7d.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # fraud-rpt
-
 The Relying Party Transmitter
 
 # Example Events
-
 TBD - Requiring extrapolation from SampleEvents.ts
--- src/common/enums/InboundPipelineURLs.ts
+++
@@ -1,5 +0,0 @@
-export enum InboundPipelineURLs {
-  development = "https://inbound-ssf.dev.account.gov.uk",
-  build = "https://inbound-ssf.build.account.gov.uk",
-  staging = "https://inbound-ssf.staging.account.gov.uk",
-}
