4ef67b33119d270ad89fb83ad93708fcc50b053a
Iimaan Mohamed <iimaan.mohamed@digital.cabinet-office.gov.uk>
2024-02-28 10:02:49 +0000

    FPAD-1788-fixing commenting issues in test.ts file

--- src/lambdas/PublicKeyLambda/handler.test.ts
+++ src/lambdas/PublicKeyLambda/handler.test.ts
@@ -1,73 +1,72 @@
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import { publicKeyLambda } from './handler';
-import { KeyManager } from '../../common/classes/keys/keys';
+import { APIGatewayProxyEvent, Context } from "aws-lambda";
+import { publicKeyLambda } from "./handler";
+import { KeyManager } from "../../common/classes/keys/keys";
 // import { LogEvents } from '../../common/enums/Log-events';
-import { KeyObject } from 'crypto';
-import { ssmParams } from '../../common/classes/SSM/SSMParams';
+import { KeyObject } from "crypto";
+import { ssmParams } from "../../common/classes/SSM/SSMParams";
 
-jest.mock('../../common/classes/keys/keys');
+jest.mock("../../common/classes/keys/keys");
 
 // jest.mock('@govuk-one-login/logging/logging', () => ({
-  // FraudLogger: jest.fn(() => ({
-    logDebug: jest.fn(),
-    logMessage: jest.fn(),
-    logErrorProcessing: jest.fn(),
-    metrics: {
-      publishStoredMetrics: jest.fn(),
-    },
-  })),
-  fraudTracer: {
-    captureAWSv3Client: () => jest.fn(),
-    captureLambdaHandler: () => jest.fn(),
-    captureMethod: () => jest.fn(),
-  },
-}));
- 
+//   FraudLogger: jest.fn(() => ({
+//     logDebug: jest.fn(),
+//     logMessage: jest.fn(),
+//     logErrorProcessing: jest.fn(),
+//     metrics: {
+//       publishStoredMetrics: jest.fn(),
+//     },
+//   })),
+//   fraudTracer: {
+//     captureAWSv3Client: () => jest.fn(),
+//     captureLambdaHandler: () => jest.fn(),
+//     captureMethod: () => jest.fn(),
+//   },
+// }));
+
 const mockKeyObject: KeyObject = {
-  type: 'public',
-  export: () => Buffer.from('mockPublicKey'),
-  asymmetricKeyType: 'rsa',
+  type: "public",
+  export: () => Buffer.from("mockPublicKey"),
+  asymmetricKeyType: "rsa",
   asymmetricKeySize: 2048,
 } as unknown as KeyObject;
-  
-describe('PublicKeyLambda', () => {
-    const mockEvent = {} as APIGatewayProxyEvent;
-    const mockContext = {} as Context;
-  
-    beforeEach(() => {
-      jest.resetAllMocks();
-      jest.spyOn(KeyManager, 'getPublicKeyFromKMS').mockImplementation();
-      jest.spyOn(ssmParams, 'getParams').mockResolvedValue(['mockPublicKeyArn']);
-    });
-  
-    it('should have the lambda defined', () => {
-      expect(publicKeyLambda).toBeDefined();
-      expect(publicKeyLambda.handler).toBeDefined();
-    });
-  
-    it('should call KeyManager.getPublicKeyFromKMS with ARN from SSM', async () => {
-      await publicKeyLambda.handler(mockEvent, mockContext);
-      expect(KeyManager.getPublicKeyFromKMS).toHaveBeenCalledWith(
-        'mockPublicKeyArn'
-      );
-    });
-  
-    it('should return a 200 status and publicKey on successful execution', async () => {
-      jest
-        .spyOn(KeyManager, 'getPublicKeyFromKMS')
-        .mockResolvedValue(mockKeyObject);
-      const response = await publicKeyLambda.handler(mockEvent, mockContext);
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toEqual('bW9ja1B1YmxpY0tleQ==');
-    });
-  
-    it('should return a 500 status and error when no key', async () => {
-      jest
-        .spyOn(KeyManager, 'getPublicKeyFromKMS')
-        .mockRejectedValue(new Error('Error'));
-      const response = await publicKeyLambda.handler(mockEvent, mockContext);
-      expect(response.statusCode).toBe(500);
-      expect(response.body).toEqual(LogEvents.PublicKeyRequestFail);
-    });
+
+describe("PublicKeyLambda", () => {
+  const mockEvent = {} as APIGatewayProxyEvent;
+  const mockContext = {} as Context;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.spyOn(KeyManager, "getPublicKeyFromKMS").mockImplementation();
+    jest.spyOn(ssmParams, "getParams").mockResolvedValue(["mockPublicKeyArn"]);
   });
-  
+
+  it("should have the lambda defined", () => {
+    expect(publicKeyLambda).toBeDefined();
+    expect(publicKeyLambda.handler).toBeDefined();
+  });
+
+  it("should call KeyManager.getPublicKeyFromKMS with ARN from SSM", async () => {
+    await publicKeyLambda.handler(mockEvent, mockContext);
+    expect(KeyManager.getPublicKeyFromKMS).toHaveBeenCalledWith(
+      "mockPublicKeyArn",
+    );
+  });
+
+  it("should return a 200 status and publicKey on successful execution", async () => {
+    jest
+      .spyOn(KeyManager, "getPublicKeyFromKMS")
+      .mockResolvedValue(mockKeyObject);
+    const response = await publicKeyLambda.handler(mockEvent, mockContext);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual("bW9ja1B1YmxpY0tleQ==");
+  });
+
+  it("should return a 500 status and error when no key", async () => {
+    jest
+      .spyOn(KeyManager, "getPublicKeyFromKMS")
+      .mockRejectedValue(new Error("Error"));
+    const response = await publicKeyLambda.handler(mockEvent, mockContext);
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual(LogEvents.PublicKeyRequestFail);
+  });
+});
--- src/lambdas/PublicKeyLambda/handler.ts
+++ src/lambdas/PublicKeyLambda/handler.ts
@@ -1,23 +1,23 @@
 import {
-    APIGatewayProxyEvent,
-    APIGatewayProxyResult,
-    Context,
-  } from 'aws-lambda';
-  //import { FraudLogger, fraudTracer } from '@govuk-one-login/logging/logging';
-  //import { Logger } from '@aws-lambda-powertools/logger';
-  import { Metrics } from '@aws-lambda-powertools/metrics';
-  import { LambdaInterface } from '@aws-lambda-powertools/commons/lib/utils/lambda';
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+// import { FraudLogger, fraudTracer } from '@govuk-one-login/logging/logging';
+// import { Logger } from '@aws-lambda-powertools/logger';
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import { LambdaInterface } from "@aws-lambda-powertools/commons/lib/utils/lambda";
 //  import { LogEvents } from '../../common/enums/Log-events';
-  import { KeyManager } from '../../common/classes/keys/keys';
-  import { SSMClient } from '@aws-sdk/client-ssm';
-  import { KeyObject } from 'crypto';
-  import { PublicKeyLambdaResponse } from '../../common/interfaces/interfaces';
-  import { ParameterNames, ssmParams } from '../../common/classes/SSM/SSMParams';
+import { KeyManager } from "../../common/classes/keys/keys";
+import { SSMClient } from "@aws-sdk/client-ssm";
+import { KeyObject } from "crypto";
+import { PublicKeyLambdaResponse } from "../../common/interfaces/interfaces";
+import { ParameterNames, ssmParams } from "../../common/classes/SSM/SSMParams";
 //  import { captureLambdaHandler } from '@aws-lambda-powertools/tracer';
-  import middy from '@middy/core';
+import middy from "@middy/core";
 
 class PublicKeyLambda implements LambdaInterface {
-//  constructor(public fraudLogger: FraudLogger) {}
+  //  constructor(public fraudLogger: FraudLogger) {}
 
   /**
    *  Triggered from APIGateway request. Retrieves public key from KMS and returns to calling client
@@ -27,52 +27,51 @@ class PublicKeyLambda implements LambdaInterface {
    */
   public async handler(
     event: APIGatewayProxyEvent,
-    context: Context
+    context: Context,
   ): Promise<APIGatewayProxyResult> {
     try {
-//      this.fraudLogger.logMessage(LogEvents.PublicKeyRequested);
+      //      this.fraudLogger.logMessage(LogEvents.PublicKeyRequested);
       const [publicKeyArn]: string[] = await ssmParams.getParams([
         ParameterNames.PublicKeyArn,
       ]);
       const publicKey: KeyObject = await KeyManager.getPublicKeyFromKMS(
-        publicKeyArn
+        publicKeyArn,
       );
       const publicKeyData: string | Buffer = publicKey.export({
-        type: 'spki',
-        format: 'pem',
+        type: "spki",
+        format: "pem",
       });
-      const publicKeyString: string = publicKeyData.toString('base64');
+      const publicKeyString: string = publicKeyData.toString("base64");
       const response: PublicKeyLambdaResponse = {
         statusCode: 200,
         body: publicKeyString,
       };
 
-//      this.fraudLogger.logMessage(LogEvents.PublicKeyReturned);
+      //  this.fraudLogger.logMessage(LogEvents.PublicKeyReturned);
       return response;
     } catch (error: any) {
-//      this.fraudLogger.logErrorProcessing(error);
+      //      this.fraudLogger.logErrorProcessing(error);
       const errorResponse: PublicKeyLambdaResponse = {
         statusCode: 500,
-//        body: LogEvents.PublicKeyRequestFail,
+        //        body: LogEvents.PublicKeyRequestFail,
       };
       return errorResponse;
     }
   }
 }
 
+// export const publicKeyLambda: PublicKeyLambda = new PublicKeyLambda(
+// //  new FraudLogger(
+//     new Logger(),
+//     new Metrics({
+//       serviceName: process.env.LAMBDA_NAME,
+//       namespace: process.env.POWERTOOLS_METRICS_NAMESPACE,
+//     }),
+//     process.env.ENVIRONMENT
+//   ),
+//   new SSMClient()
+// );
 
-export const publicKeyLambda: PublicKeyLambda = new PublicKeyLambda(
-//  new FraudLogger(
-    new Logger(),
-    new Metrics({
-      serviceName: process.env.LAMBDA_NAME,
-      namespace: process.env.POWERTOOLS_METRICS_NAMESPACE,
-    }),
-    process.env.ENVIRONMENT
-  ),
-  new SSMClient()
-);
-
-export const handler = middy(publicKeyLambda.handler.bind(publicKeyLambda)).use(
-//  captureLambdaHandler(fraudTracer)
-);
+// export const handler = middy(publicKeyLambda.handler.bind(publicKeyLambda)).use(
+// //  captureLambdaHandler(fraudTracer)
+// );
